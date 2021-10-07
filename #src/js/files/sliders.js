e924bccf79fa92bd7cee4982576ebd04646ae6ab
Herman461 <zhukovskiyherman@gmail.com>
2021-10-07 23:06:01 +0400

    v0.3

--- #src/js/files/sliders.js
+++ #src/js/files/sliders.js
@@ -40,7 +40,7 @@ const mainslider = new Swiper('.mainslider__body', {
       el: '.footer-mainslider__progressbar',
       type: 'progressbar',
    },
-	navigation: {
+   navigation: {
       nextEl: '.footer-mainslider__arrow_r',
       prevEl: '.footer-mainslider__arrow_l',
    },
@@ -92,6 +92,10 @@ const mainslider = new Swiper('.mainslider__body', {
    // },
 });
 
+function addZero(num) {
+   return num > 9 ? num : '0' + num;
+}
+
 const mainsliderAllSlides = document.querySelector('.fraction-mainslider__total');
 const mainsliderCurrentSlide = document.querySelector('.fraction-mainslider__current');
 mainsliderAllSlides.innerHTML = mainslider.slides.length;
@@ -101,25 +105,149 @@ mainslider.on('slideChange', function () {
    mainsliderCurrentSlide.innerHTML = currentSlide;
 });
 
+const popularProductsAllSlides = document.querySelector(
+   '.popular-products .slider-line__total',
+);
+const popularProductsCurrentSlide = document.querySelector(
+   '.popular-products .slider-line__current',
+);
+
 const popularProductsSlider = new Swiper('.popular-products__body', {
    pagination: {
-      el: '.popular-products__slider-line .slider-line__progressbar',
+      el: '.popular-products .slider-line__progressbar',
       type: 'progressbar',
    },
-	navigation: {
-      nextEl: '.slider-line__arrow_next',
-      prevEl: '.popular-products__slider-line .slider-line__arrow_prev',
+   navigation: {
+      nextEl: '.popular-products .slider-line__arrow_next',
+      prevEl: '.popular-products .slider-line__arrow_prev',
    },
-	slidesPerView: 3,
-	spaceBetween: 30,
+   spaceBetween: 25,
+   breakpoints: {
+      992: {
+         slidesPerView: 3,
+         spaceBetween: 30,
+      },
+      630: {
+         slidesPerView: 2,
+         spaceBetween: 30,
+      },
+      380: {
+         slidesPerView: 1.3,
+      },
+   },
+   on: {
+      init: function () {
+         popularProductsAllSlides.innerHTML = addZero(
+            Math.ceil(this.slides.length - this.params.slidesPerView + 1),
+         );
+      },
+      slideChange: function () {
+         let currentSlide = ++this.realIndex;
+         popularProductsCurrentSlide.innerHTML = addZero(currentSlide);
+      },
+      resize: function () {
+         popularProductsAllSlides.innerHTML = addZero(
+            Math.ceil(this.slides.length - this.params.slidesPerView + 1),
+         );
+      },
+   },
 });
 
-const popularProductsAllSlides = document.querySelector('.popular-products__slider-line .slider-line__total');
-const popularProductsCurrentSlide = document.querySelector('.popular-products__slider-line .slider-line__current');
-popularProductsAllSlides.innerHTML = 9 < popularProductsSlider.slides.length - 2 ? (popularProductsSlider.slides.length - 2) : "0" + (popularProductsSlider.slides.length - 2);
+const ourProjectsAllSlides = document.querySelector('.our-projects .slider-line__total');
+const ourProjectsCurrentSlide = document.querySelector('.our-projects .slider-line__current');
 
-popularProductsSlider.on('slideChange', function () {
-   let currentSlide = ++popularProductsSlider.realIndex;
-   popularProductsCurrentSlide.innerHTML = 9 < currentSlide ? currentSlide : "0" + currentSlide;
+const ourProjectsSlider = new Swiper('.our-projects__body', {
+   slidesPerView: 1,
+   direction: 'horizontal',
+   pagination: {
+      el: '.our-projects .slider-line__progressbar',
+      type: 'progressbar',
+   },
+   navigation: {
+      nextEl: '.our-projects .slider-line__arrow_next',
+      prevEl: '.our-projects .slider-line__arrow_prev',
+   },
+   spaceBetween: 27,
+   on: {
+      init: function () {
+         ourProjectsAllSlides.innerHTML = addZero(
+            Math.ceil(this.slides.length - this.params.slidesPerView + 1),
+         );
+      },
+      slideChange: function () {
+         let currentSlide = ++this.realIndex;
+         ourProjectsCurrentSlide.innerHTML = addZero(currentSlide);
+      },
+      resize: function () {
+         ourProjectsAllSlides.innerHTML = addZero(
+            Math.ceil(this.slides.length - this.params.slidesPerView + 1),
+         );
+      },
+   },
 });
 
+const ourPartnersAllSlides = document.querySelector('.our-partners .slider-line__total');
+const ourPartnersCurrentSlide = document.querySelector('.our-partners .slider-line__current');
+
+let ourPartnersSlider = new Swiper('.our-partners__body', {
+	slidesPerColumn: 2,
+	pagination: {
+		el: '.our-partners .slider-line__progressbar',
+		type: 'progressbar',
+	},
+	navigation: {
+		nextEl: '.our-partners .slider-line__arrow_next',
+		prevEl: '.our-partners .slider-line__arrow_prev',
+	},
+	breakpoints: {
+		992: {
+			slidesPerView: 4,
+			slidesPerColumn: 2,
+		},
+		600: {
+			slidesPerView: 3,
+			slidesPerColumn: 2,
+		},
+		414: {
+			slidesPerView: 2,
+			slidesPerColumn: 2,
+		},
+	},
+	on: {
+		init: function () {
+			console.log(this);
+			ourPartnersAllSlides.innerHTML = addZero(
+				Math.ceil((this.slides.length / 2) - this.params.slidesPerView + 1),
+			);
+		},
+		slideChange: function () {
+			let currentSlide = ++this.realIndex;
+			ourPartnersCurrentSlide.innerHTML = addZero(currentSlide);
+		},
+		resize: function () {
+			console.log(this)
+			ourPartnersAllSlides.innerHTML = addZero(
+				Math.ceil((this.slides.length / 2) - this.params.slidesPerView + 1),
+			);
+		},
+	},
+});
+
+const ourPartnersBody = document.querySelector('.our-partners__body');
+const ourPartnersSliderLine = document.querySelector('.our-partners .slider-line');
+if (window.matchMedia('(min-width: 991.98px)').matches) {
+   ourPartnersBody.classList.add('disabled');
+   ourPartnersSliderLine.classList.add('disabled');
+	
+}
+window.addEventListener('resize', function () {
+   if (window.matchMedia('(min-width: 991.98px)').matches) {
+      ourPartnersBody.classList.add('disabled');
+   } else {
+      if (ourPartnersBody.classList.contains('disabled')) {
+         ourPartnersBody.classList.remove('disabled');
+         ourPartnersSliderLine.classList.remove('disabled');
+      }
+   }
+
+});
